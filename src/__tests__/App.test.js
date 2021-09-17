9b1aa78df9ca397d65b9a800a6e70ee611537ae9
Brian Thomas <btdeveloper125@gmail.com>
2021-09-17 14:08:06 -0600

    feat: Add CitySearch - beforeAll

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -2,11 +2,20 @@ import React from 'react'
 import { shallow } from 'enzyme'
 import App from '../App'
 import EventList from '../EventList'
+import CitySearch from '../CitySearch'
 
 // create new scope called <App /> component
 describe('<App /> component', () => {
+  // use beforeall to keep code DRY
+  let AppWrapper
+  beforeAll(() => {
+    AppWrapper = shallow(<App />)
+  })
   test('render list of events', () => {
-    const AppWrapper = shallow(<App />)
     expect (AppWrapper.find(EventList)).toHaveLength(1)
+  })
+
+  test('render CitySearch', () => {
+    expect(AppWrapper.find(CitySearch)).toHaveLength(1)
   })
 })

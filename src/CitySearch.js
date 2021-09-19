60d06e6ce4d525a16187f4c9539e2d7d8a8a88df
Brian Thomas <btdeveloper125@gmail.com>
2021-09-19 11:59:44 -0600

    feat: Setup CC for mock data

--- src/CitySearch.js
+++ src/CitySearch.js
@@ -1,11 +1,25 @@
-import React, { useEffect } from "react"
+import React, { Component } from 'react';
+class CitySearch extends Component {
+  constructor() {
+    super()
+    this.state = {
+      query: 'Munich'
+    }
+  }
 
-function CitySearch() {
-  return (
-    <div className='CitySearch'>
-      
-    </div>
-  )
+  render() {
+    return (
+      <div className='CitySearch'>
+        <input
+          type='text'
+          className='city'
+          // pass the value of state into input
+          value={this.state.query}
+        />
+        <ul className="suggestions"></ul>
+      </div>
+    )
+  }
 }
 
 export default CitySearch

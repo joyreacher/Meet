531328e689a9a7284cf899c60ebad87e0bc06c11
Brian Thomas <btdeveloper125@gmail.com>
2021-09-22 09:31:24 -0600

    feat: Add new Comp - NumberOfComponents

---
+++ src/NumberOfEvents.js
@@ -0,0 +1,13 @@
+import React, { useEffect } from "react"
+
+export default function NumberOfEvents() {
+  return (
+    <div>
+        <p>Number of events</p>
+        <input
+          data-test='text-box'
+          type='text'
+        />
+    </div>
+  )
+}

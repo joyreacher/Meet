95979ba5712f208fc6275458ab85629e6b05ff72
Brian Thomas <btdeveloper125@gmail.com>
2021-09-22 10:33:50 -0600

    refactor: Update to pass tests and run in browser

--- src/__tests__/Event.test.js
+++ src/__tests__/Event.test.js
@@ -11,42 +11,43 @@ describe('<Event /> component', () => {
   const findDataTest = (wrapper, value) => wrapper.find(`[data-test="${value}"]`)
 
   beforeAll(() => {
-    EventItem = shallow(<Event event={mockData} />)
-    EventItemMount = mount(<Event event={mockData} />)
+    // using the 1st item in the mockData array to
+    // simulate how the app behaves in the browser
+    EventItem = shallow(<Event event={mockData[0]} />)
   })
 
-  test('correct number of events are displayed', () => {
-    const numberOfEvents = findDataTest(EventItem, 'event')
-    expect(numberOfEvents.length).toBe(2)
-  })
+  // test('correct number of events are displayed', () => {
+  //   const numberOfEvents = findDataTest(EventItem, 'event')
+  //   expect(numberOfEvents.length).toBe(2)
+  // })
 
   test('Event element is collapsed by default', () => {
     const collapsedEvents = findDataTest(EventItem, 'event-details-hide')
-    expect(collapsedEvents.length).toBe(2)
+    expect(collapsedEvents.length).toBe(1)
   })
 
   test('Match event description', () => {
-    const eventDescription = findDataTest(EventItemMount, 'event-description')
-    expect(eventDescription.at(0).text()).toMatch(mockData[0].description)
+    const eventDescription = findDataTest(EventItem, 'event-description')
+    expect(eventDescription.first().text()).toMatch(mockData[0].description)
   })
 
   test('Match event location', () => {
-    const eventLocation = findDataTest(EventItemMount, 'event-location')
-    expect(eventLocation.at(0).text()).toMatch(mockData[0].location)
+    const eventLocation = findDataTest(EventItem, 'event-location')
+    expect(eventLocation.first().text()).toMatch(mockData[0].location)
   })
 
   test('Event element can expand to reveal details', () => {
-    const eventToExpand = findDataTest(EventItemMount, 'event-title')
+    const eventToExpand = findDataTest(EventItem, 'event-title')
     eventToExpand.at(0).simulate('click')
-    const numberOfExpandedEvents = findDataTest(EventItemMount, 'event-details-show')
-    expect(numberOfExpandedEvents.length).toBe(2)
+    const numberOfExpandedEvents = findDataTest(EventItem, 'event-details-show')
+    expect(numberOfExpandedEvents.length).toBe(1)
   })
 
   test('Event element can be collapsed', () => {
-    const collapsedEvent = findDataTest(EventItemMount, 'event-title')
+    const collapsedEvent = findDataTest(EventItem, 'event-title')
     collapsedEvent.at(0).simulate('click')
-    const numberOfCollapsedEvents = findDataTest(EventItemMount, 'event-details-hide')
-    expect(numberOfCollapsedEvents.length).toBe(2)
+    const numberOfCollapsedEvents = findDataTest(EventItem, 'event-details-hide')
+    expect(numberOfCollapsedEvents.length).toBe(1)
   })
   
 })

5980453d2773a83a76051d1239be9d14e3943e70
Brian Thomas <btdeveloper125@gmail.com>
2021-09-22 10:34:55 -0600

    refactor: Update - remove .map event not object

--- src/Event.js
+++ src/Event.js
@@ -3,16 +3,15 @@ import React, { useState } from 'react'
 export default function Event({ event }) {
   const [menu, setMenu] = useState(false)
   return (
-    event.map(item => {
-      return (
-        <div data-test='event' className='Event' key={item.id}>
-          <h1 data-test='event-title' onClick={() => {!menu ? setMenu(true) : setMenu(false)}}>{item.summary}</h1>
-          <div data-test={`event-details-${!menu ? 'hide' : 'show'}`} onClick={() => {!menu ? setMenu(true) : setMenu(false)}} className={`Event-details ${!menu ? 'hide-details' : 'show-details'}`}>
-            <p data-test='event-description'>{item.description}</p>
-            <p data-test='event-location'>{item.location}</p>
-          </div>
-        </div>
-      )
-    })
+    <div data-test='event' className='Event' key={event.id}>
+      <h1 data-test='event-title' onClick={() => {!menu ? setMenu(true) : setMenu(false)}}>{event.summary}</h1>
+      <div
+        data-test={`event-details-${!menu ? 'hide' : 'show'}`}
+        onClick={() => {!menu ? setMenu(true) : setMenu(false)}}
+        className={`Event-details ${!menu ? 'hide-details' : 'show-details'}`}>
+        <p data-test='event-description'>{event.description}</p>
+        <p data-test='event-location'>{event.location}</p>
+      </div>
+    </div>
   )
 }

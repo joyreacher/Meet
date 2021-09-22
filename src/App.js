4dfed4e05e214d564147d9a74cb542588c4f4d55
Brian Thomas <btdeveloper125@gmail.com>
2021-09-22 10:30:51 -0600

    refactor: mockData added to run in browser

--- src/App.js
+++ src/App.js
@@ -3,13 +3,14 @@ import './App.css';
 import EventList from './EventList';
 import CitySearch from './CitySearch';
 import NumberOfEvents from './NumberOfEvents';
-
+// mock data to run application in browser
+import { mockData } from './mock-data';
 function App() {
   return (
     <div className="App">
       <NumberOfEvents />
       <CitySearch />
-      <EventList />
+      <EventList events={mockData} />
     </div>
   );
 }

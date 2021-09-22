39f8d51efef623a954779c7deb8459545b048e5c
Brian Thomas <btdeveloper125@gmail.com>
2021-09-22 17:00:27 -0600

    refactor: Update  to access state in tests

--- src/NumberOfEvents.js
+++ src/NumberOfEvents.js
@@ -1,13 +1,38 @@
-import React, { useEffect } from "react"
+import React, { Component } from "react"
 
-export default function NumberOfEvents() {
-  return (
-    <div>
-        <p>Number of events</p>
+class NumberOfEvents extends Component {
+  constructor() {
+    super()
+    this.state = {
+      query: 32
+    }
+  }
+
+  handleInputChanged = (e) => {
+    const mockValue = 16
+    this.setState({
+      query: mockValue
+    })
+    this.state.query = e.target.value
+  }
+
+  render() {
+    const { inputValue } = this.props
+    return (
+      <div>
+        <p data-test='number'>Number of events</p>
         <input
+          name='events'
           data-test='text-box'
           type='text'
+          value={this.state.query}
+          onChange={(e) => {
+            this.handleInputChanged(e)
+          }}
         />
-    </div>
-  )
+      </div>
+    )
+  }
+  
 }
+export default NumberOfEvents

df4aab4a1ed18127adf3018292de575813e9c43e
Brian Thomas <btdeveloper125@gmail.com>
2021-09-23 09:24:18 -0600

    refactor: Removed 2 uncovered lines

--- src/Event.js
+++ src/Event.js
@@ -1,14 +1,11 @@
 import React, { useState } from 'react'
 
-export default function Event({ event }) {
+export default function Event ({ event }) {
   const [menu, setMenu] = useState(false)
   return (
     <div data-test='event' className='Event' key={event.id}>
       <h1 data-test='event-title' onClick={() => {!menu ? setMenu(true) : setMenu(false)}}>{event.summary}</h1>
-      <div
-        data-test={`event-details-${!menu ? 'hide' : 'show'}`}
-        onClick={() => {!menu ? setMenu(true) : setMenu(false)}}
-        className={`Event-details ${!menu ? 'hide-details' : 'show-details'}`}>
+      <div data-test={`event-details-${!menu ? 'hide' : 'show'}`} onClick={() => {!menu ? setMenu(true) : setMenu(false)}} className={`Event-details ${!menu ? 'hide-details' : 'show-details'}`}>
         <p data-test='event-description'>{event.description}</p>
         <p data-test='event-location'>{event.location}</p>
       </div>

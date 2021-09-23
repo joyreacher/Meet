434cc9163317bc1a6e89d25e8028f2b23960a6b6
Brian Thomas <btdeveloper125@gmail.com>
2021-09-23 13:56:49 -0600

    refactor: Remove error

--- src/__tests__/CitySearch.test.js
+++ src/__tests__/CitySearch.test.js
@@ -8,7 +8,7 @@ describe('<CitySearch /> component', () => {
   let CitySearchWrapper, locations
   beforeAll(() => {
     locations = extractLocations(mockData)
-    CitySearchWrapper = shallow(<CitySearch locations={locations} />)
+    CitySearchWrapper = shallow(<CitySearch locations={locations} updateEvents={() => {}} />)
   })
   test('render text input', () => {
     // check whether an element with the class name of city exists within the CitySearchWrapper component

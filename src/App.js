9b0342293fff682bcb4038141f84a6021f377dab
Brian Thomas <btdeveloper125@gmail.com>
2021-09-23 12:48:24 -0600

    refactor: Update to class component for tests

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import EventList from './EventList';
 import CitySearch from './CitySearch';
@@ -7,14 +7,23 @@ import NumberOfEvents from './NumberOfEvents';
 import { mockData } from './mock-data';
 import { extractLocations } from './api'
 const locations = extractLocations(mockData)
-function App() {
-  return (
-    <div className="App">
-      <NumberOfEvents events={mockData} />
-      <CitySearch locations={locations} />
-      <EventList events={mockData} />
-    </div>
-  );
+class App extends Component {
+  constructor(){
+    super()
+    this.state = {
+      events: mockData
+    }
+  }
+
+  render(){
+    return (
+      <div className="App">
+        <NumberOfEvents events={mockData} />
+        <CitySearch locations={locations} />
+        <EventList events={this.state.events} />
+      </div>
+    );
+  }
 }
 
 export default App;

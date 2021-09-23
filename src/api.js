4f6ddc1641ed158e53f10c32b7ad2f837cb9917a
Brian Thomas <btdeveloper125@gmail.com>
2021-09-23 13:53:48 -0600

    feat: Add getEvents to import events

--- src/api.js
+++ src/api.js
@@ -1,3 +1,9 @@
+import { mockData } from "./mock-data"
+
+export const getEvents = async () => {
+  return mockData
+}
+
 export const extractLocations = events => {
   let extractLocations = events.map(event => event.location)
   let locations = [...new Set(extractLocations)]

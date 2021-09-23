dc5553d8bcddb654b87d6607920a3b60ea63d42c
Brian Thomas <btdeveloper125@gmail.com>
2021-09-23 13:06:54 -0600

    feat: Add integration test  eventList & citySearch

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,9 +1,34 @@
 import React from 'react'
-import { shallow } from 'enzyme'
+import { shallow, mount } from 'enzyme'
 import App from '../App'
 import EventList from '../EventList'
 import CitySearch from '../CitySearch'
 import NumberOfEvents from '../NumberOfEvents'
+
+// new scope - App Integration
+describe('<App /> integration', () => {
+  // make sure that EventList gets events as a prop
+  test('App passes "events" state as a prop to EventList', () => {
+    // set the wrapper
+    const AppWrapper = mount(<App />)
+    // set the state uksing the wrapper
+    const AppEventsState = AppWrapper.state('events')
+    // test - state is NOT undefined
+    expect(AppEventsState).not.toEqual(undefined)
+    // test -EventList props to equal the events state
+    expect(AppWrapper.find(EventList).props().events).toEqual(AppEventsState)
+    // clean up DOM
+    AppWrapper.unmount()
+  })
+
+  test('App passes "locations" state as a prop to CitySearch', () => {
+    const AppWrapper = mount(<App />)
+    const AppLocationsState = AppWrapper.state('locations')
+    expect(AppLocationsState).not.toEqual(undefined)
+    expect(AppWrapper.find(CitySearch).props().locations).toBe(AppLocationsState)
+    AppWrapper.unmount()
+  })
+})
 
 // create new scope called <App /> component
 describe('<App /> component', () => {

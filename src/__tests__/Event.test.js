a363f7c4495a5ae0142abc42a3e4762d29fd382c
Brian Thomas <btdeveloper125@gmail.com>
2021-09-23 09:22:42 -0600

    feat: Test event summary

--- src/__tests__/Event.test.js
+++ src/__tests__/Event.test.js
@@ -5,31 +5,26 @@ import { mockData } from '../mock-data'
 
 describe('<Event /> component', () => {
   let EventItem
-
-  // Helper function
   const findDataTest = (wrapper, value) => wrapper.find(`[data-test="${value}"]`)
-
   beforeAll(() => {
-    // using the 1st item in the mockData array to
-    // simulate how the app behaves in the browser
-    EventItem = shallow(<Event event={mockData[0]} />)
+    EventItem = shallow(<Event event={mockData[0]}/>)
   })
-
   test('Event element is collapsed by default', () => {
     const collapsedEvents = findDataTest(EventItem, 'event-details-hide')
     expect(collapsedEvents.length).toBe(1)
   })
-
   test('Match event description', () => {
     const eventDescription = findDataTest(EventItem, 'event-description')
     expect(eventDescription.first().text()).toMatch(mockData[0].description)
   })
-
   test('Match event location', () => {
     const eventLocation = findDataTest(EventItem, 'event-location')
     expect(eventLocation.first().text()).toMatch(mockData[0].location)
   })
-
+  test('Check text of event title', () => {
+    const eventTitle = findDataTest(EventItem, 'event-title')
+    expect(eventTitle.text()).toBe(mockData[0].summary)
+  })
   test('Event element can expand to reveal details', () => {
     const eventToExpand = findDataTest(EventItem, 'event-title')
     eventToExpand.at(0).simulate('click')

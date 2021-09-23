c95b0afe94c737a0dbef643c2e51914a2adb7051
Brian Thomas <btdeveloper125@gmail.com>
2021-09-23 13:42:17 -0600

    feat: Add method to change event state

--- src/App.js
+++ src/App.js
@@ -5,21 +5,31 @@ import CitySearch from './CitySearch';
 import NumberOfEvents from './NumberOfEvents';
 // mock data to run application in browser
 import { mockData } from './mock-data';
-import { extractLocations } from './api'
+import { extractLocations, getEvents } from './api'
 const locations = extractLocations(mockData)
 class App extends Component {
   constructor(){
     super()
     this.state = {
-      events: mockData
+      events: mockData,
+      locations: locations
     }
+  }
+
+  updateEvents = (location) => {
+    getEvents().then(events => {
+      const locationEvents = events.filter(event => event.location === location)
+      this.setState({
+        events: locationEvents
+      })
+    })
   }
 
   render(){
     return (
       <div className="App">
         <NumberOfEvents events={mockData} />
-        <CitySearch locations={locations} />
+        <CitySearch locations={this.state.locations} />
         <EventList events={this.state.events} />
       </div>
     );

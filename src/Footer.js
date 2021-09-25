de70d5798a43b2e405e054e68dff9c76e5b9d733
Brian Thomas <btdeveloper125@gmail.com>
2021-09-25 11:47:13 -0600

    feat: Add page nav/footer

---
+++ src/Footer.js
@@ -0,0 +1,13 @@
+import React, { useEffect } from "react"
+
+function Footer() {
+  return (
+    <div>
+      <p>Github</p>
+      <p>LinkedIN</p>
+      <p>Email</p>
+    </div>
+  )
+}
+
+export default Footer

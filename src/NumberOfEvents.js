d6b407989c80b759b5af27e164baac3c2c49f85b
Brian Thomas <btdeveloper125@gmail.com>
2021-10-07 12:41:53 -0600

    refactor: Update to rely on state

--- src/NumberOfEvents.js
+++ src/NumberOfEvents.js
@@ -11,92 +11,36 @@ class NumberOfEvents extends Component {
     }
   }
 
-  async componentDidMount() {
-    await this.setState({
+  componentDidMount() {
+    this.setState({
       events: this.props.events,
-      locations: this.props.locations
+      locations: this.props.locations,
+      query: this.props.number
     })
   }
   
-  handleInputChanged = (e) => {
-    const value = e.target.value
-    // const suggestions = this.props.locations.filter((location) => {
-    //   if(value === ''){
-    //     return false
-    //   }
-    //   return location
-    // })
-    // check for location value
-    
-    // if no location given return the specified number of events by date
-    
-    // if location if given search the events array for events matching location
-    
-    // number
-    // const dates = []
-    
-    
-    // for(let i = 0; i < e.target.value; i++){
-      // console.log(this.state.events[i])
-    // }
-    this.setState({
-      query: value
-    })
-    return this.props.updateEvents(this.props.locations, value)
-    
-    // const numberOfEvents = this.state.events.map((element, index )=> {
-    //   let date = new Date(element.start.dateTime)
-      // date.toUTCString()
-      // console.log(date.toDateString())
-      // console.log(date.toDateString())
-      // dates.push(date.toDateString())
-      
-      // return element
-      // });
-    // sort dates 
-    // console.log(dates)
-    // console.log(numberOfEvents)
-    
-    // match date to id 
-    
-    // display event
-    
-    
-    // element
-    // const intersection = this.state.events.filter((element, index )=> {
-      // this.state.locations.forEach(location => {
-
-      // })
-      
-      // if(element.location === 'Berlin, Germany') {
-      //   return element
-      // }
-      
-      
-      // this.state.locations.includes()
-      // });
-    // this.setState({
-    //   query: value
-    // })
-  }
 
   render() {
     const { events, number } = this.props
+    console.log(number)
     return (
       <div className='NumberOfEvents'>
         <p data-test='number'>Number of events</p>
         <div>
           <p data-test='number-of-events'>
-          {this.state.events.length}
+            {!number ? events.length : number}
           </p>
         </div>
         <input
           name='events'
           data-test='text-box'
           type='text'
-          value={!number ? this.state.query : number}
+          value={number}
           onChange={(e) => {
-            this.handleInputChanged(e)
+            this.setState({
+              query: e.target.value
+            })
+            return this.props.updateEvents(this.props.locations, e.target.value)
           }}
         />
       </div>

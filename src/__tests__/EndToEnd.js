40919e6709d2f288bde532928e4d7b83ec53f65a
Brian Thomas <btdeveloper125@gmail.com>
2021-10-11 13:22:54 -0600

    feat: Add end to end for 2nd feature

--- src/__tests__/EndToEnd.js
+++ src/__tests__/EndToEnd.js
@@ -1,16 +1,33 @@
 import puppeteer from 'puppeteer'
 
 describe('show/hide an event details', () => {
-  test('An event element is collapsed by default', async () => {
-    const browser = await puppeteer.launch()
-    
-    const page = await browser.newPage()
+  let browser, page
+  beforeAll( async () => {
+    // jest.setTimeout(30000)
+    browser = await puppeteer.launch()
+    page = await browser.newPage()
     await page.goto('http://localhost:3000/')
-
     await page.waitForSelector('.Event')
-    
+  })
+  afterAll(() => {
+    browser.close()
+  })
+
+  test('An event element is collapsed by default', async () => {
     const eventDetails = await page.$('.Event .event-details')
     expect(eventDetails).toBeNull()
-    browser.close()
+  })
+
+  test('User can expand an event to see its details', async () => {
+    await page.click('.Event .event-title')
+    const eventDetails = await page.$('.Event .event-details')
+    expect(eventDetails).toBeDefined()
+  })
+
+  test('User can collapse an event to hide its details', async () => {
+    await page.click('.Event .event-title')
+    const eventDetails = await page.$('.Event .event-details')
+    // toBeNull matcher is used to ensure the extra details element no longer exists
+    expect(eventDetails).toBeNull()
   })
 })

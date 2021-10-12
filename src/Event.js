3b972e1394346b1da9958d7161f48356f4a1286c
Brian Thomas <btdeveloper125@gmail.com>
2021-10-12 15:22:25 -0600

    feat: Add open/close icons update test

--- src/Event.js
+++ src/Event.js
@@ -7,8 +7,8 @@ export default function Event ({ event }) {
   const [menu, setMenu] = useState(false)
   return (
     <div className='Event' data-test='event' key={event.id}>
-      <h1 data-test='event-title' className='event-title'>{event.summary}</h1>
-      <FontAwesomeIcon className='event-icon' data-test='event-icon' icon={!menu ? faChevronCircleDown : faChevronCircleUp} onClick={() => { !menu ? setMenu(true) : setMenu(false) }} />
+      <h1 data-test='event-title' className='event-title' >{event.summary}</h1>
+      <FontAwesomeIcon className={`event-icon event-icon-${!menu ? 'hide' : 'show'}`} data-test='event-icon' icon={!menu ? faChevronCircleDown : faChevronCircleUp} onClick={() => { !menu ? setMenu(true) : setMenu(false) }} />
       <div data-test={`event-details-${!menu ? 'hide' : 'show'}`} className={`event-details-${!menu ? 'hide' : 'show'}`}>
         <p className='event-description' data-test='event-description'>{event.description}</p>
         <p className='event-link'>{event.htmlLink}</p>
--- src/__tests__/EndToEnd.js
+++ src/__tests__/EndToEnd.js
@@ -3,8 +3,13 @@ import puppeteer from 'puppeteer'
 describe('show/hide an event details', () => {
   let browser, page
   beforeAll( async () => {
-    // jest.setTimeout(30000)
+    jest.setTimeout(30000)
     browser = await puppeteer.launch()
+    // browser = await puppeteer.launch({
+    //   headless: false,
+    //   slowMo: 250,
+    //   ignoreDefaultArgs: ['--disable-extensions']
+    // })
     page = await browser.newPage()
     await page.goto('http://localhost:3000/')
     await page.waitForSelector('.Event')
@@ -14,18 +19,18 @@ describe('show/hide an event details', () => {
   })
 
   test('An event element is collapsed by default', async () => {
-    const eventDetails = await page.$('.Event .event-details')
+    const eventDetails = await page.$('.Event .event-icon-show')
     expect(eventDetails).toBeNull()
   })
 
   test('User can expand an event to see its details', async () => {
-    await page.click('.Event .event-title')
-    const eventDetails = await page.$('.Event .event-details')
+    await page.click('.Event .event-icon-hide')
+    const eventDetails = await page.$('.Event .event-icon-show')
     expect(eventDetails).toBeDefined()
   })
 
   test('User can collapse an event to hide its details', async () => {
-    await page.click('.Event .event-title')
+    await page.click('.Event .event-icon-show')
     const eventDetails = await page.$('.Event .event-details')
     // toBeNull matcher is used to ensure the extra details element no longer exists
     expect(eventDetails).toBeNull()

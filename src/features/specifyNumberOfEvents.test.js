2150dbc2dc145783b90b871dd26ea3eb6614b8e7
Brian Thomas <btdeveloper125@gmail.com>
2021-10-13 10:57:52 -0600

    test: Number of events not specifed

--- src/features/specifyNumberOfEvents.test.js
+++ src/features/specifyNumberOfEvents.test.js
@@ -1,5 +1,10 @@
-// import Jest Cucumber
 import { loadFeature, defineFeature } from "jest-cucumber";
+import { mount } from 'enzyme'
+// components
+import App from '../App'
+import NumberOfEvents from '../NumberOfEvents';
+// data
+import { mockData } from '../mock-data';
 // use loadFeature to read the .feature text file
 const feature = loadFeature('./src/features/specifyNumberOfEvents.feature')
 // use defineFeature to create a test suite
@@ -7,8 +12,12 @@ defineFeature(feature, test => {
   // Feature file has a scenario titled "When user hasn’t specified a number, 32 is the default number", but no match found in step definitions. Try adding the following code:
 
     test('When user hasn’t specified a number, 32 is the default number', ({ given, when, then }) => {
-        given('the user has not entered anything in the number of events field', () => {
-
+      let AppWrapper
+        given('the user has not entered anything in the number of events field', async () => {
+          AppWrapper = await mount(<App events={mockData} updateEvents={() => {}} />)
+          expect(AppWrapper.state('numberOfEvents')).toBe(32)
+          const NumberOfEventsWrapper = await mount(<NumberOfEvents updateEvents={() => {}} events={mockData} number={AppWrapper.state('numberOfEvents')} />)
+          expect(NumberOfEventsWrapper.prop('number')).toBe(32)
         });
 
         when('the user is looking for all events in a given location', () => {

fb52c5f3cfb3f111a5ab309167d2a5a3c36168dd
Brian Thomas <btdeveloper125@gmail.com>
2021-10-14 09:02:45 -0600

    refactror: Update to validate event object

--- src/Event.js
+++ src/Event.js
@@ -5,16 +5,21 @@ import { faChevronCircleDown, faChevronCircleUp } from '@fortawesome/free-solid-
 
 export default function Event ({ event }) {
   const [menu, setMenu] = useState(false)
-  return (
-    <div className='Event' data-test='event' key={event.id}>
-      <h1 data-test='event-title' className='event-title' >{event.summary}</h1>
-      <FontAwesomeIcon className={`event-icon event-icon-${!menu ? 'hide' : 'show'}`} data-test='event-icon' icon={!menu ? faChevronCircleDown : faChevronCircleUp} onClick={() => { !menu ? setMenu(true) : setMenu(false) }} />
-      <div data-test={`event-details-${!menu ? 'hide' : 'show'}`} className={`event-details-${!menu ? 'hide' : 'show'}`}>
-        <p className='event-description' data-test='event-description'>{event.description}</p>
-        <p className='event-link'>{event.htmlLink}</p>
-        <p className='event-date' data-test='event-date'>{new Date(event.start.dateTime).toDateString()}</p>
-        <p className='event-location' data-test='event-location'>{event.location}</p>
+  if(!event){
+    return false
+  } else {
+    return (
+      <div className='Event' data-test='event' key={event.id}>
+        <h1 data-test='event-title' className='event-title' >{event.summary}</h1>
+        <FontAwesomeIcon className={`event-icon event-icon-${!menu ? 'hide' : 'show'}`} data-test='event-icon' icon={!menu ? faChevronCircleDown : faChevronCircleUp} onClick={() => { !menu ? setMenu(true) : setMenu(false) }} />
+        <div data-test={`event-details-${!menu ? 'hide' : 'show'}`} className={`event-details-${!menu ? 'hide' : 'show'}`}>
+          <p className='event-description' data-test='event-description'>{event.description}</p>
+          <p className='event-link'>{event.htmlLink}</p>
+          <p className='event-date' data-test='event-date'>{new Date(event.start.dateTime).toDateString()}</p>
+          <p className='event-location' data-test='event-location'>{event.location}</p>
+        </div>
       </div>
-    </div>
-  )
+    )
+  }
+  
 }

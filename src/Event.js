92d111db475e3e751b1d7adf5de92c59bf90e513
Brian Thomas <btdeveloper125@gmail.com>
2021-10-20 08:45:26 -0600

    refactor: Update event card

--- src/Event.js
+++ src/Event.js
@@ -9,11 +9,13 @@ export default function Event ({ event }) {
   } else {
     return (
       <div className='Event' data-test='event' key={event.id}>
-        <h1 data-test='event-title' className='event-title'>{event.summary}</h1>
-        <FontAwesomeIcon className={`event-icon event-icon-${!menu ? 'hide' : 'show'}`} data-test='event-icon' icon={!menu ? faChevronCircleDown : faChevronCircleUp} onClick={() => { !menu ? setMenu(true) : setMenu(false) }} />
+        <div className='event-title-container'>
+          <h1 data-test='event-title' className='event-title'>{event.summary}</h1>
+          <FontAwesomeIcon className={`event-icon event-icon-${!menu ? 'hide' : 'show'}`} data-test='event-icon' icon={!menu ? faChevronCircleDown : faChevronCircleUp} onClick={() => { !menu ? setMenu(true) : setMenu(false) }} />
+        </div>
         <div data-test={`event-details-${!menu ? 'hide' : 'show'}`} className={`event-details-${!menu ? 'hide' : 'show'}`}>
           <p className='event-description' data-test='event-description'>{event.description}</p>
-          <p className='event-link'>{event.htmlLink}</p>
+          <a href={event.htmlLink} className='event-link'>{event.htmlLink}</a>
           <p className='event-date' data-test='event-date'>{new Date(event.start.dateTime).toDateString()}</p>
           <p className='event-location' data-test='event-location'>{event.location}</p>
         </div>

2f344848f253d68785034908cecf132cddccb8b0
Brian Thomas <btdeveloper125@gmail.com>
2021-10-29 14:01:38 -0600

    feat: Add attribute for svg art

--- src/Footer.js
+++ src/Footer.js
@@ -6,6 +6,7 @@ function Footer () {
       <div className='footer__container'>
         <a className='footer__link'  href='https://github.com/joyreacher/Meet'>Github</a>
         <a className='footer__link' href='https://www.linkedin.com/in/brianthomas--web-developer/'>LinkedIn</a>
+        <a className='footer__link' href="https://www.vectorstock.com/royalty-free-vector/people-in-bar-cafe-cartoon-couple-friends-man-vector-35145920">Vector image by VectorStock / vectorstock</a>
       </div>
     </div>
   )

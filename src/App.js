578e28404081e18b078464e55d0fb9b6717f5cf9
Brian Thomas <btdeveloper125@gmail.com>
2021-11-02 14:59:54 -0600

    feat: Install/Add Recharts

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import React, { Component, lazy, Suspense } from 'react';
+import React, { PureComponent, Component, lazy, Suspense } from 'react';
+import { ScatterChart, Scatter, Line, ZAxis, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
 import './CSS/styles.css';
 // import CitySearch from './CitySearch';
 // import NumberOfEvents from './NumberOfEvents';
@@ -12,6 +13,71 @@ const CitySearch = lazy(() => import('./CitySearch'));
 const NumberOfEvents = lazy(() => import('./NumberOfEvents'));
 const WelcomeScreen = lazy(() => import('./WelcomeScreen'));
 const Illustration = lazy(() => import('./Illustration'));
+
+const data01 = [
+  {
+    "x": 100,
+    "y": 200,
+    "z": 200
+  },
+  {
+    "x": 120,
+    "y": 100,
+    "z": 260
+  },
+  {
+    "x": 170,
+    "y": 300,
+    "z": 400
+  },
+  {
+    "x": 140,
+    "y": 250,
+    "z": 280
+  },
+  {
+    "x": 150,
+    "y": 400,
+    "z": 500
+  },
+  {
+    "x": 110,
+    "y": 280,
+    "z": 200
+  }
+];
+const data02 = [
+  {
+    "x": 200,
+    "y": 260,
+    "z": 240
+  },
+  {
+    "x": 240,
+    "y": 290,
+    "z": 220
+  },
+  {
+    "x": 190,
+    "y": 290,
+    "z": 250
+  },
+  {
+    "x": 198,
+    "y": 250,
+    "z": 210
+  },
+  {
+    "x": 180,
+    "y": 280,
+    "z": 260
+  },
+  {
+    "x": 210,
+    "y": 220,
+    "z": 230
+  }
+];
 
 class App extends Component {
   constructor(){
@@ -30,53 +96,53 @@ class App extends Component {
     }
   }
   
-  //
-  // async componentDidMount() {
-  //   this.mounted = true
-  //   this.setState({ showWelcomeScreen: false })
-  //   getEvents().then(events => {
-  //     if (this.mounted){
-  //       this.setState({
-  //         events, locations: extractLocations(events)
-  //         })
-  //       if(!this.state.numberOfEvents){
-  //         this.updateEvents([], 32)
-  //       }
-  //     }
-  //   })
-  // }
-
+  
   async componentDidMount() {
     this.mounted = true
-    
-    let testConnection = await checkOnlineStatus()
-      if (testConnection.status !== 200) {
+    this.setState({ showWelcomeScreen: false })
+    getEvents().then(events => {
+      if (this.mounted){
         this.setState({
-          onlineErr: 'Offline'
-        })
-      }
-      const accessToken = localStorage.getItem('access_token')
-      const isTokenValid = (await checkToken(accessToken)).error ? false : true
-      const searchParams = new URLSearchParams(window.location.search)
-      const code = searchParams.get('code')
-      this.setState({ showWelcomeScreen: !(code || isTokenValid )})
-      console.log(!(code || isTokenValid))
-    if((code || isTokenValid) && this.mounted){
-      if(this.state.onlineErr !== ''){
-        this.setState({showWelcomeScreen: true})
-      }
-      getEvents().then(events => {
-        if (this.mounted){
-          this.setState({
-            events, locations: extractLocations(events)
-            })
-          if(!this.state.numberOfEvents){
-            this.updateEvents([], 32)
-          }
+          events, locations: extractLocations(events)
+          })
+        if(!this.state.numberOfEvents){
+          this.updateEvents([], 32)
         }
-      })
-    }
+      }
+    })
   }
+
+  // async componentDidMount() {
+  //   this.mounted = true
+    
+  //   let testConnection = await checkOnlineStatus()
+  //     if (testConnection.status !== 200) {
+  //       this.setState({
+  //         onlineErr: 'Offline'
+  //       })
+  //     }
+  //     const accessToken = localStorage.getItem('access_token')
+  //     const isTokenValid = (await checkToken(accessToken)).error ? false : true
+  //     const searchParams = new URLSearchParams(window.location.search)
+  //     const code = searchParams.get('code')
+  //     this.setState({ showWelcomeScreen: !(code || isTokenValid )})
+  //     console.log(!(code || isTokenValid))
+  //   if((code || isTokenValid) && this.mounted){
+  //     if(this.state.onlineErr !== ''){
+  //       this.setState({showWelcomeScreen: true})
+  //     }
+  //     getEvents().then(events => {
+  //       if (this.mounted){
+  //         this.setState({
+  //           events, locations: extractLocations(events)
+  //           })
+  //         if(!this.state.numberOfEvents){
+  //           this.updateEvents([], 32)
+  //         }
+  //       }
+  //     })
+  //   }
+  // }
 
   componentWillUnmount() {
     this.mounted = false
@@ -182,6 +248,16 @@ class App extends Component {
     })
   }
 
+  getData = () => {
+    const { locations, events } = this.state
+    const data = locations.map((location) => {
+      const number = events.filter(event => event.location === location).length
+      const city = location.split(', ').shift()
+      return { city, number }
+    })
+    console.log(data)
+    return data
+  }
   render(){
     const renderLoader = () => <p className='Alert'>Loading</p>;
     if(this.state.showWelcomeScreen === undefined){
@@ -205,9 +281,22 @@ class App extends Component {
               </Suspense>
             </div>
           </div>
-          <Suspense fallback={renderLoader}>
-            <EventList events={this.state.events} />
-          </Suspense>
+          <div>
+            <ScatterChart width={800} height={450}
+              margin={{ top: 20, right: 40, bottom: 10, left: 10 }}>
+              <CartesianGrid strokeDasharray="3 3" />
+              <XAxis type='category' dataKey="city" name="City" />
+              <YAxis type='number' dataKey="number" name="Events" allowDecimals={false}/>
+              <ZAxis dataKey="z" range={[64, 144]} name="score" unit="km" />
+              <Tooltip cursor={{ strokeDasharray: '3 3' }} />
+              <Legend />
+              <Scatter name="Events" data={this.getData()} fill="#82ca9d" />
+              </ScatterChart>
+            <Suspense fallback={renderLoader}>
+              <EventList events={this.state.events} />
+            </Suspense>
+          </div>
+          
         </div>
         {/* <Footer /> */}
         <Suspense fallback={renderLoader}>

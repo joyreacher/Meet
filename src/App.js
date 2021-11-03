0034aade638ddad156af846b40ec6220424db89f
Brian Thomas <btdeveloper125@gmail.com>
2021-11-03 10:26:21 -0600

    feat: Add scatter chart

--- src/App.js
+++ src/App.js
@@ -1,83 +1,18 @@
 import React, { PureComponent, Component, lazy, Suspense } from 'react';
-import { ScatterChart, Scatter, Line, ZAxis, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+import { PieChart, Pie, Sector, Cell, ScatterChart, Scatter, LabelList, Line, ZAxis, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
 import './CSS/styles.css';
 // import CitySearch from './CitySearch';
 // import NumberOfEvents from './NumberOfEvents';
 import Navbar from './Navbar';
+import PieChartGraph from './pieChart'
 // mock data to run application in browser
 import { checkOnlineStatus, checkToken, extractLocations, getAccessToken, getEvents } from './api'
-// import WelcomeScreen from './WelcomeScreen';
 import { OnlineAlert } from './Alert';
 import EventList from './EventList';
 const CitySearch = lazy(() => import('./CitySearch'));
 const NumberOfEvents = lazy(() => import('./NumberOfEvents'));
 const WelcomeScreen = lazy(() => import('./WelcomeScreen'));
 const Illustration = lazy(() => import('./Illustration'));
-
-const data01 = [
-  {
-    "x": 100,
-    "y": 200,
-    "z": 200
-  },
-  {
-    "x": 120,
-    "y": 100,
-    "z": 260
-  },
-  {
-    "x": 170,
-    "y": 300,
-    "z": 400
-  },
-  {
-    "x": 140,
-    "y": 250,
-    "z": 280
-  },
-  {
-    "x": 150,
-    "y": 400,
-    "z": 500
-  },
-  {
-    "x": 110,
-    "y": 280,
-    "z": 200
-  }
-];
-const data02 = [
-  {
-    "x": 200,
-    "y": 260,
-    "z": 240
-  },
-  {
-    "x": 240,
-    "y": 290,
-    "z": 220
-  },
-  {
-    "x": 190,
-    "y": 290,
-    "z": 250
-  },
-  {
-    "x": 198,
-    "y": 250,
-    "z": 210
-  },
-  {
-    "x": 180,
-    "y": 280,
-    "z": 260
-  },
-  {
-    "x": 210,
-    "y": 220,
-    "z": 230
-  }
-];
 
 class App extends Component {
   constructor(){
@@ -96,53 +31,52 @@ class App extends Component {
     }
   }
   
-  
-  async componentDidMount() {
-    this.mounted = true
-    this.setState({ showWelcomeScreen: false })
-    getEvents().then(events => {
-      if (this.mounted){
-        this.setState({
-          events, locations: extractLocations(events)
-          })
-        if(!this.state.numberOfEvents){
-          this.updateEvents([], 32)
-        }
-      }
-    })
-  }
-
+  //! FOR TESTING
   // async componentDidMount() {
   //   this.mounted = true
-    
-  //   let testConnection = await checkOnlineStatus()
-  //     if (testConnection.status !== 200) {
+  //   this.setState({ showWelcomeScreen: false })
+  //   getEvents().then(events => {
+  //     if (this.mounted){
   //       this.setState({
-  //         onlineErr: 'Offline'
-  //       })
-  //     }
-  //     const accessToken = localStorage.getItem('access_token')
-  //     const isTokenValid = (await checkToken(accessToken)).error ? false : true
-  //     const searchParams = new URLSearchParams(window.location.search)
-  //     const code = searchParams.get('code')
-  //     this.setState({ showWelcomeScreen: !(code || isTokenValid )})
-  //     console.log(!(code || isTokenValid))
-  //   if((code || isTokenValid) && this.mounted){
-  //     if(this.state.onlineErr !== ''){
-  //       this.setState({showWelcomeScreen: true})
-  //     }
-  //     getEvents().then(events => {
-  //       if (this.mounted){
-  //         this.setState({
-  //           events, locations: extractLocations(events)
-  //           })
-  //         if(!this.state.numberOfEvents){
-  //           this.updateEvents([], 32)
-  //         }
+  //         events, locations: extractLocations(events)
+  //         })
+  //       if(!this.state.numberOfEvents){
+  //         this.updateEvents([], 32)
   //       }
-  //     })
-  //   }
+  //     }
+  //   })
   // }
+
+  async componentDidMount() {
+    this.mounted = true
+    
+    let testConnection = await checkOnlineStatus()
+      if (testConnection.status !== 200) {
+        this.setState({
+          onlineErr: 'Offline'
+        })
+      }
+      const accessToken = localStorage.getItem('access_token')
+      const isTokenValid = (await checkToken(accessToken)).error ? false : true
+      const searchParams = new URLSearchParams(window.location.search)
+      const code = searchParams.get('code')
+      this.setState({ showWelcomeScreen: !(code || isTokenValid )})
+    if((code || isTokenValid) && this.mounted){
+      if(this.state.onlineErr !== ''){
+        this.setState({showWelcomeScreen: true})
+      }
+      getEvents().then(events => {
+        if (this.mounted){
+          this.setState({
+            events, locations: extractLocations(events)
+            })
+          if(!this.state.numberOfEvents){
+            this.updateEvents([], 32)
+          }
+        }
+      })
+    }
+  }
 
   componentWillUnmount() {
     this.mounted = false
@@ -255,11 +189,11 @@ class App extends Component {
       const city = location.split(', ').shift()
       return { city, number }
     })
-    console.log(data)
     return data
   }
   render(){
     const renderLoader = () => <p className='Alert'>Loading</p>;
+    const data = this.getData()
     if(this.state.showWelcomeScreen === undefined){
       return (
         <div className="App" />
@@ -281,24 +215,32 @@ class App extends Component {
               </Suspense>
             </div>
           </div>
-          <div>
-            <ScatterChart width={800} height={450}
-              margin={{ top: 20, right: 40, bottom: 10, left: 10 }}>
-              <CartesianGrid strokeDasharray="3 3" />
-              <XAxis type='category' dataKey="city" name="City" />
-              <YAxis type='number' dataKey="number" name="Events" allowDecimals={false}/>
-              <ZAxis dataKey="z" range={[64, 144]} name="score" unit="km" />
-              <Tooltip cursor={{ strokeDasharray: '3 3' }} />
-              <Legend />
-              <Scatter name="Events" data={this.getData()} fill="#82ca9d" />
+          <div className='chart'>
+            <ResponsiveContainer  height="100%">
+              <ScatterChart
+                margin={{
+                  top: 0,
+                  right: 0,
+                  bottom: 25,
+                  left: -20,
+                }}
+              >
+                <CartesianGrid stroke="#F4F7E9" fill='hsla(217, 62%, 39%, .55)'/>
+                <XAxis type="category" dataKey="city" name="city" stroke='#eaf8bf'/>
+                <YAxis type="number" dataKey="number" name="number" allowDecimals={false} stroke='#eaf8bf'/>
+                <Tooltip cursor={{ strokeDasharray: '8 8'}} active={true}/>
+                <Scatter name="Events" data={data} fill='#20488A' animationBegin={500}>
+                  <LabelList dataKey="number" position='top' stroke='#eaf8bf' fill='#eaf8bf'/>
+                </Scatter>
               </ScatterChart>
+            </ResponsiveContainer>
+          </div>
+            
+            
             <Suspense fallback={renderLoader}>
               <EventList events={this.state.events} />
             </Suspense>
-          </div>
-          
         </div>
-        {/* <Footer /> */}
         <Suspense fallback={renderLoader}>
           <WelcomeScreen showWelcomeScreen={this.state.showWelcomeScreen} getAccessToken={() => getAccessToken() }/>
         </Suspense>

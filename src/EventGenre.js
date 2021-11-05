1ba3e7f0143c48ade2d41c362e3ef24d277ed642
Brian Thomas <btdeveloper125@gmail.com>
2021-11-05 09:42:52 -0600

    refacotr: Updated pie graph container

--- src/EventGenre.js
+++ src/EventGenre.js
@@ -1,15 +1,11 @@
 import React, { useEffect, Suspense } from "react"
-import { PieChart, Pie, Sector, Cell, ScatterChart, Scatter, LabelList, Line, ZAxis, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+import { ResponsiveContainer } from 'recharts';
 import CircleGraph from './pieChart'
 function EventGenre({events}) {
   return (
-    <>
-    <ResponsiveContainer width='100%'>
-      <Suspense fallback={<p>wtf</p>}>
-        <CircleGraph style={{'margin': '0 auto'}} events={events}/>
-      </Suspense>
+    <ResponsiveContainer className='container__graph-circle' width='100%' height='100%'>
+        <CircleGraph events={events}/>
     </ResponsiveContainer>
-    </>
   )
 }
 
